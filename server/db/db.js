3a5318d3fcd19df2fbd8f1088e93f43331506cfb
peter <peterhanggg@gmail.com>
2021-08-04 13:33:22 -0400

    updated db config

--- server/db/db.js
+++ server/db/db.js
@@ -1,12 +1,8 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const db = new Sequelize(
-  process.env.DATABASE_URL ||
-  "postgres://postgres:postgres@localhost:5432/messenger",
-  {
-    logging: false,
-  }
-);
+const db = new Sequelize(process.env.DATABASE_URL, {
+  logging: false,
+});
 
 module.exports = db;
